8d0a59418cfd5623633cb5935958f015a8c3f289
Fernando Salamanca <fesaba21@gmail.com>
2024-03-18 17:20:27 -0500

    gitignore

--- src/app.ts
+++ src/app.ts
@@ -8,8 +8,7 @@ const port = 3000;
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
-
-app.use(cors({ origin: "*"}))
+//app.use(cors({ origin: "http://localhost:3000", credentials: true }))
 
 app.get('/', (req, res) => {
     res.send('Hello World!');
