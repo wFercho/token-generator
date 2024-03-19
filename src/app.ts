db03532277332f5a5d76c2d01d8c6dad5cc913b0
Fernando Salamanca <fesaba21@gmail.com>
2024-03-19 01:15:36 -0500

    remove allow explicit cors conf

--- src/app.ts
+++ src/app.ts
@@ -8,7 +8,7 @@ const port = 3000;
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
-app.use(cors({ origin: "*" }))
+app.use(cors())
 
 app.get('/', (req, res) => {
     res.send('Hello World!');
